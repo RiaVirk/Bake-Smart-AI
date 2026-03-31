352c182f5194ecdd6ddf7def9bdaffb216bfd957
RiaVirk <virkmariaofficial@gmail.com>
2026-03-31 14:40:20 +0200

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>Bake Smart AI</h2>
-      <h3>Maria Virk</h3>
-    </div>
-  );
+  redirect('/sales')
 }
